2f477c1d12b5cbb915614d5b0aeeb820e1f75e04
TYRANNY905 <mudra.rudra123@gmail.com>
2026-04-26 12:20:19 +0530

    Add login API with JWT authentication

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 // REGISTER
 export const register = async (req, res) => {
@@ -27,6 +28,45 @@ export const register = async (req, res) => {
 
     res.status(201).json({
       message: "User registered successfully",
+      user: userData,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+};
+
+// LOGIN
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // check user
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // create token
+    const token = jwt.sign(
+      { userId: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      },
+    );
+
+    // remove password
+    const { password: _, ...userData } = user._doc;
+
+    res.json({
+      message: "Login successful",
+      token,
       user: userData,
     });
   } catch (error) {
